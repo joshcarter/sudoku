6f81dc7a170f4e07c01707bd99b92903969d14c7
Josh Carter <josh@multipart-mixed.com>
2008-12-18 20:52:10 -0700

    initial commit

---
+++ grid.rb
@@ -0,0 +1,207 @@
+class Solved < Exception
+  attr_reader :cells
+  
+  def initialize(cells)
+    @cells = cells
+  end
+end
+
+class Unsolvable < Exception
+end
+
+class Grid
+  def initialize(cells)
+    # Need to do a deep copy of the cells array
+    @cells = Marshal.load(Marshal.dump(cells))
+    @dimension = (@cells[0].length)
+    @stride = Math::sqrt(@dimension).to_i
+    @possible_values = (1..@dimension).to_a
+  end
+
+  def self.load(filename)
+    lines = File::readlines(filename)
+
+    cells = lines.map do |line|
+      line.split.map do |cell| 
+        cell == '_' ? nil : cell.to_i
+      end
+    end
+    
+    Grid.new(cells)
+  end
+  
+  def save(filename)
+    File::open(filename, "w") do |file|
+      file.print self
+    end
+  end
+
+  def to_s
+    str = String.new
+    
+    each_cell_with_index do |cell, row, col|
+      str << (cell ? cell.to_s : '_')
+      str << (col == @dimension - 1 ? "\n" : " ")
+    end
+
+    str
+  end
+  
+  def get(row, col)
+    @cells[row][col]
+  end
+  
+  def set(row, col, value)
+    @cells[row][col] = value
+  end
+  
+  def each_cell
+    @cells.each do |row|
+      row.each do |cell|
+        yield cell
+      end
+    end
+  end
+  
+  def each_cell_with_index
+    @cells.each_with_index do |row, row_num|
+      row.each_with_index do |cell, col_num|
+        yield cell, row_num, col_num
+      end
+    end
+  end
+
+  def solved?
+    each_cell do |cell|
+      return false if cell.nil?
+    end
+    
+    return true
+  end
+  
+  def solvable?
+    each_cell_with_index do |cell, row, col|
+      return false if (cell.nil? and unused_values_for_cell(row, col) == [])
+    end
+    
+    return true
+  end
+
+  def unused_values_for_row(row)
+    values = []
+
+    each_cell_with_index do |cell, current_row, current_col|
+      values << cell unless cell.nil? or current_row != row
+    end
+
+    @possible_values - values
+  end
+
+  def unused_values_for_col(col)
+    values = []
+
+    each_cell_with_index do |cell, current_row, current_col|
+      values << cell unless cell.nil? or current_col != col
+    end
+
+    @possible_values - values
+  end
+
+  def zone_for(row, col)
+    [row / @stride, col / @stride]
+  end
+
+  def unused_values_for_zone(row, col)
+    values = []
+    requested_zone = zone_for(row, col)
+
+    each_cell_with_index do |cell, current_row, current_col|
+      values << cell unless cell.nil? or
+                            zone_for(current_row, current_col) != requested_zone
+    end
+
+    @possible_values - values
+  end
+  
+  def unused_values_for_cell(row, col)
+    unused_values_for_row(row) &
+    unused_values_for_col(col) &
+    unused_values_for_zone(row, col)
+  end
+  
+  def solve_determinate_cells
+    loop do
+      changes = 0
+      
+      each_cell_with_index do |cell, row, col|
+        if cell.nil?
+          possible_values = unused_values_for_cell(row, col)
+
+          if (possible_values.length == 1)
+            # puts "solve_determinate_cells: (#{row}, #{col}) = #{possible_values.first}"
+            set(row, col, possible_values.first)
+            changes += 1
+          end
+        end
+      end
+
+      break if changes == 0
+    end
+  end
+  
+  def solve_with_guesses
+    solve_determinate_cells
+
+    raise Unsolvable unless solvable?
+
+    # Create a list of cells that are empty, and the number of possible
+    # values for each
+    open_cells = Array.new
+    each_cell_with_index do |cell, row, col|
+      if cell.nil?
+        open_cells << { :row => row, :col => col, :num_values => unused_values_for_cell(row, col).length }
+      end
+    end
+
+    # Sort that list so that the cells with least possible values are first
+    open_cells.sort_by { |tuple| tuple[:num_values] }
+
+    first_cell = open_cells.first
+    puts "starting with cell (#{first_cell[:row]}, #{first_cell[:col]}) because it has #{first_cell[:num_values]} values"
+
+    open_cells.each do |tuple|
+      row = tuple[:row]
+      col = tuple[:col]
+      possible_values = unused_values_for_cell(row, col)
+
+      # puts "possible values for (#{row}, #{col}): #{possible_values.join(', ')}"
+
+      possible_values.each do |value|
+        begin
+          puts "trying (#{row}, #{col}) = #{value}"
+
+          new_grid = Grid::new(@cells)
+          new_grid.set(row, col, value)
+          puts new_grid
+          new_grid.solve_with_guesses
+
+          # Bail out to top-level solve
+          raise Solved.new(new_grid.instance_variable_get(:@cells))
+        rescue Unsolvable
+          puts "unsolvable"
+        end
+      end
+    end
+
+    raise Unsolvable unless solved?
+  end
+  
+  def solve
+    begin
+      solve_with_guesses
+    rescue Solved => e
+      # Copy over cells from solved grid
+      @cells = e.cells
+    end
+  end
+end
---
+++ test.rb
@@ -0,0 +1,108 @@
+require 'test/unit'
+require 'grid'
+
+class GridTester < Test::Unit::TestCase
+  def test_grid_valid
+    grid = Grid::load("grids/easy_grid.txt")
+    assert_equal(9, grid.get(0, 0))
+    assert_equal(8, grid.get(0, 1))
+    assert_equal(nil, grid.get(1, 0))
+  end
+  
+  def test_unused_values_for_row
+    grid = Grid::load("grids/easy_grid.txt")
+    assert_equal(grid.unused_values_for_row(0), [1, 2, 4, 5, 7])
+    assert_equal(grid.unused_values_for_row(2), [1, 6, 7, 8])
+  end
+  
+  def test_unused_values_for_col
+    grid = Grid::load("grids/easy_grid.txt")
+    assert_equal(grid.unused_values_for_col(2), [5, 6, 7])
+    assert_equal(grid.unused_values_for_col(0), [2, 4, 6, 7])
+  end
+  
+  def test_zone_for
+    grid = Grid::load("grids/easy_grid.txt")
+    assert_equal(grid.zone_for(0, 0), [0, 0])
+    assert_equal(grid.zone_for(1, 0), [0, 0])
+    assert_equal(grid.zone_for(2, 0), [0, 0])
+    assert_equal(grid.zone_for(8, 0), [2, 0])
+  end
+  
+  def test_unused_values_for_zone
+    grid = Grid::load("grids/easy_grid.txt")
+    assert_equal(grid.unused_values_for_zone(0, 0), [1, 4, 5, 6, 7])
+    assert_equal(grid.unused_values_for_zone(0, 8), [1, 6, 7, 8])
+    assert_equal(grid.unused_values_for_zone(8, 8), [1, 4, 5, 6, 7])
+  end
+  
+  def test_unused_values_for_cell
+    grid = Grid::load("grids/easy_grid.txt")
+    assert_equal(grid.unused_values_for_cell(0, 2), [5, 7])
+    assert_equal(grid.unused_values_for_cell(1, 0), [4, 6])
+    assert_equal(grid.unused_values_for_cell(1, 1), [1])
+    assert_equal(grid.unused_values_for_cell(1, 2), [6])
+    assert_equal(grid.unused_values_for_cell(2, 0), [6, 7])
+
+    # Fill in cells with only one valid option
+    grid.set(1, 1, 1)
+    grid.set(1, 2, 6)
+    
+    assert_equal(grid.unused_values_for_cell(1, 0), [4])
+    grid.set(1, 0, 4)
+    
+    assert_equal(grid.unused_values_for_cell(2, 0), [7])
+    grid.set(2, 0, 7)
+    
+    assert_equal(grid.unused_values_for_cell(0, 2), [5])
+    grid.set(0, 2, 5)
+  end
+  
+  def test_solve_easy_grid
+    puts "running easy grid"
+    grid = Grid::load("grids/easy_grid.txt")
+    grid.solve
+
+    assert_equal true, grid.solved?
+    
+    puts "solved grid:"
+    puts grid.to_s
+  end
+  
+  def test_grid_solvable
+    grid = Grid::load("grids/medium_grid.txt")
+    assert_equal true, grid.solvable?
+    assert_equal [2, 8, 9], grid.unused_values_for_cell(1,3)
+    
+    grid.set(7,3,8)
+    grid.set(8,3,9)
+    grid.set(0,4,2)
+    
+    assert_equal [], grid.unused_values_for_cell(1,3)
+    assert_equal false, grid.solvable?
+  end
+  
+  def test_solve_medium_grid
+    puts "running medium grid"
+    grid = Grid::load("grids/medium_grid.txt")
+
+    # cheating: solve part the of puzzle
+    # grid.set(0,1,6)
+    # grid.set(0,4,2)
+    # grid.set(0,5,7)
+    # grid.set(0,7,9)
+    # grid.set(0,8,8)
+
+    grid.solve
+    puts "solved grid:"
+    puts grid.to_s
+   end
+   
+   # def test_solve_hard_grid
+   #   puts "running hard grid"
+   #   grid = Grid::load("grids/hard_grid.txt")
+   #   grid.solve
+   #   puts "solved grid:"
+   #   puts grid.to_s
+   # end
+end
